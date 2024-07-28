cfce3a788334fc237df94029c3972b13e4bd2702
Manuel De Luzi <manuel.deluzi@globalservs.com>
2024-07-28 18:01:30 +0200

    Implement basic Flask server for setting service

---
+++ settings_service/server/main.py
@@ -0,0 +1,17 @@
+from settings import SETTINGS
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/settings/messaging/<framework>', methods=['GET'])
+def get_messaging_settings(framework):
+    return jsonify(SETTINGS['messaging'][framework])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+    
+
+    
---
+++ settings_service/server/settings.py
@@ -0,0 +1,21 @@
+
+
+SETTINGS = {
+    'messaging': {
+        "rabbitmq": {
+            "host": "localhost",
+            "port": 5672,
+            "username": "guest",
+            "password": "guest",
+            "vhost": "/"
+        },
+        "redis": {
+            "host": "localhost",
+            "port": 6379,
+            "db": 0
+        }
+    }
+}
+    
+    
+
